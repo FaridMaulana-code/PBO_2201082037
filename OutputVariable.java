a9e18f1bac9eeb64566c5e1045956c55f21c6311
MUHAMMAD FARID MAULANA <126837116+FaridMaulana-code@users.noreply.github.com>
2023-03-06 13:23:22 +0700

    memasukan OutputVariabel.java

---
+++ OutputVariable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FaridM030323;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class OutputVariable 
+{
+    public static void main (String[] args)
+    {
+        int value = 10;
+        char x;
+        x = 'A';
+        
+        System.out.println(value);
+        System.out.println("The Value of x=" + x);
+    }
+}

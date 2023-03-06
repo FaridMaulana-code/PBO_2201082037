51ef65c07dbfea2f54372b12c0ebcb4177b9dcd7
MUHAMMAD FARID MAULANA <126837116+FaridMaulana-code@users.noreply.github.com>
2023-03-06 13:24:02 +0700

    memasukan VariabelSample.java

---
+++ VarialbelSamples.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FaridM030323;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class VarialbelSamples 
+{
+  public static void main( String[] args) {
+      //deklarasi a tipe data dengan nama variabel
+      // result dan tipe data boolean
+      boolean result;
+      //deklarasi tipe data dengan nama variabel
+      //option dan tipe data char
+      char option;
+      option = 'c';  //menandai 'c' sebagai option
+      
+      //deklarasi tipe data dengan nama variabel
+      //grade, double tipe data dan telagh di inisialisasi
+      //to 0.0
+      double grade = 0.0;
+  }     
+}
